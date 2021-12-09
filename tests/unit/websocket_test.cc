21d2235178c5b810660b556785f05466b550253b
kozakusek <bk418339@students.mimuw.edu.pl>
2021-12-09 01:08:38 +0100

    [WS]: Add echo handler test

--- tests/unit/websocket_test.cc
+++ tests/unit/websocket_test.cc
@@ -20,7 +20,7 @@ SEASTAR_TEST_CASE(test_websocket_handshake) {
                 "Connection: Upgrade\r\n"
                 "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
                 "Sec-WebSocket-Version: 13\r\n"
-                "WebSocket-Subprotocol: echo\r\n"
+                "Sec-WebSocket-Protocol: echo\r\n"
                 "\r\n";
         loopback_connection_factory factory;
         loopback_socket_impl lsi(factory);
@@ -32,6 +32,10 @@ SEASTAR_TEST_CASE(test_websocket_handshake) {
         auto output = sock.output();
 
         websocket::server dummy;
+        dummy.register_handler("echo", [] (input_stream<char>& in,
+                    output_stream<char>& out) {
+            return make_ready_future<>();
+        });
         websocket::connection conn(dummy, acceptor.get0().connection);
         future<> serve = conn.process();
 
@@ -63,22 +67,62 @@ SEASTAR_TEST_CASE(test_websocket_handshake) {
         for (auto& header : resp->_headers) {
             std::cout << header.first << ':' << header.second << std::endl;
         }
+    });
+}
 
-        dummy.register_handler("echo", [] (input_stream<char>& in,
-                output_stream<char>& out) {
-            return make_ready_future<>();
+SEASTAR_TEST_CASE(test_websocket_handler_registration) {
+    return seastar::async([] {
+        loopback_connection_factory factory;
+        loopback_socket_impl lsi(factory);
+
+        auto acceptor = factory.get_server_socket().accept();
+        auto connector = lsi.connect(socket_address(), socket_address());
+        connected_socket sock = connector.get0();
+        auto input = sock.input();
+        auto output = sock.output();
+
+        // Setup server
+        websocket::server ws;
+        ws.register_handler("echo", [] (input_stream<char>& in,
+                    output_stream<char>& out) {
+            // TODO: Fix along with echo handler in demo.
+            return out.write("TEST");
         });
+        websocket::connection conn(ws, acceptor.get0().connection);
+        future<> serve = conn.process();
 
-        const std::string frame =
-            "\x01\x8C\x00\x00"
-            "\x00\x00\x48\x65"
-            "\x6c\x6c\x6f\x20"
-            "\x77\x6f\x72\x6c"
-            "\x64\x21";
-        const std::string response = frame;
-        
-        // TODO: actually write the test.
-        output.write(frame).get();
+        auto close = defer([&conn, &input, &output, &serve] () noexcept {
+            conn.shutdown();
+            input.close().get();
+            output.close().get();
+            serve.get();
+         });
+
+        // handshake
+        const std::string request =
+                "GET / HTTP/1.1\r\n"
+                "Upgrade: websocket\r\n"
+                "Connection: Upgrade\r\n"
+                "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
+                "Sec-WebSocket-Version: 13\r\n"
+                "Sec-WebSocket-Protocol: echo\r\n"
+                "\r\n";
+        output.write(request).get();
         output.flush().get();
+        input.read().get();
+        // Sending and recieving a websocket frame
+        const std::string ws_frame =
+            "\201\204"  // 1000 0001 1000 0100
+            "TEST"      // Masking Key
+            "\0\0\0\0"; // Masked Message - TEST
+        const std::string rs_frame =
+            "\201\004" // 1000 0001 0000 0100
+            "TEST";    // Message - TEST
+        output.write(ws_frame).get();
+        output.flush().get();
+        auto response = input.read().get();
+        auto response_str = std::string(response.begin(), response.end());
+                
+        BOOST_REQUIRE_EQUAL(rs_frame, response_str);
     });
 }
